79c30a7c386950dbdc6abf12266a2392a3d4c102
Dagger <avocado@ovacoda.com>
2021-11-15 00:49:06 -0700

    UPDATE commit vendor folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'grav-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'cloudstek/grav-plugin-webpack-manifest',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cloudstek/grav-plugin-webpack-manifest' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'grav-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
